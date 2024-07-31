3464bc7d203de697a80340f388aac29d929976c3
Jacob <jacobmrobison472@gmail.com>
2024-07-30 20:19:28 -0400

    importing progress

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const html_routes = require('./routes/html-routes.js');
+const api_routes = require('./routes/api-routes.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(api_routes);
+app.use(html_routes);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is now running on http://localhost:${PORT}`)
+});
